74fc94bf105778ef83d123cf212d097bd23f7a9c
NiccoloTosato <niccolo.tosato@yahoo.it>
2022-01-31 18:22:38 +0100

    added foo test for const iterator

--- exam/main.cpp
+++ exam/main.cpp
@@ -1,20 +1,22 @@
 #include "stack_pool.hpp"
 #include <iostream>
 
-int& foo(int& i) {
-  return i;
+
+
+
+//usefull function to check const iterator ! 
+void foo( stack_pool<int,std::size_t>& pool,std::size_t s2){
+  for(auto i=pool.begin(s2);i!=pool.end(s2);++i){
+    auto b=pool.begin(s2);
+    i=b;
+    std::cout<<*i<<std::endl;
+  }
 }
-
-
-
-
-
 
 int main() {
   stack_pool<int, std::size_t> pool{22};
   auto s = pool.new_stack();
   auto s2 = pool.new_stack();
-  int val = 3;
   s = pool.push(3, s);
   s2 = pool.push(2, s2);
   std::cout << pool.value(s2) << std::endl;
@@ -33,15 +35,12 @@ int main() {
   std::cout << pool.value(s2) << std::endl;
   std::cout << "free: " << pool.get_free_nodes() << std::endl;
 
-  // while (s) {
-  // std::cout << pool.value(s) << std::endl;
-  // }
-
   std::cout << "s: " << s << std::endl;
   std::cout << "s2: " << s2 << std::endl;
+
+
   std::cout<<"\nFOR RANGE LOOP\n";
-  for(auto i=pool.begin(s2);i!=pool.end(s2);++i)
-    std::cout<<*i<<std::endl;
+  foo(pool,s2);
     
   return 0;
 }
